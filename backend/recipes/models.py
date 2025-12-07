78fd7597cfc436708d11d62c0ceed3ae65c8ed1b
Alex Trifonov <ta.alexashow@ya.ru>
2025-12-07 23:41:11 +0300

    Ingredient

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    """Модель ингредиента."""
+
+    name = models.CharField(
+        max_length=128,
+        verbose_name="Название",
+        db_index=True,
+    )
+    measurement_unit = models.CharField(
+        max_length=64,
+        verbose_name="Единица измерения",
+    )
+
+    class Meta:
+        verbose_name = "Ингредиент"
+        verbose_name_plural = "Ингредиенты"
+        ordering = ["name"]
+        constraints = [
+            models.UniqueConstraint(
+                fields=["name", "measurement_unit"], name="unique_ingredient"
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.name} ({self.measurement_unit})"
