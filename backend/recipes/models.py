9b9589606ef0f9e7d7539effebea77f80495434e
Alex Trifonov <ta.alexashow@ya.ru>
2025-12-07 23:50:52 +0300

    Recipe

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
@@ -24,3 +26,43 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.measurement_unit})"
+
+
+class Recipe(models.Model):
+    """Модель рецепта."""
+
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="recipes",
+        verbose_name="Автор",
+        db_index=True,
+    )
+    name = models.CharField(
+        max_length=256,
+        verbose_name="Название",
+    )
+    image = models.ImageField(
+        upload_to="recipes/images/",
+        verbose_name="Изображение",
+    )
+    text = models.TextField(
+        verbose_name="Описание",
+    )
+    cooking_time = models.PositiveIntegerField(
+        verbose_name="Время приготовления (мин)",
+        validators=[MinValueValidator(1)],
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата публикации",
+        db_index=True,
+    )
+
+    class Meta:
+        verbose_name = "Рецепт"
+        verbose_name_plural = "Рецепты"
+        ordering = ["-pub_date"]
+
+    def __str__(self):
+        return self.name
