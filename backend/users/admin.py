f612228b123dbfdfd2b8de06c6640ddbd2690800
Alex Trifonov <ta.alexashow@ya.ru>
2025-12-15 18:18:47 +0300

    Фикс админок с пользователями

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.html import mark_safe
 
 from .models import Subscription, User
 
@@ -9,10 +10,12 @@ class UserAdmin(BaseUserAdmin):
     list_display = (
         "id",
         "username",
+        "full_name",
         "email",
-        "first_name",
-        "last_name",
-        "is_staff",
+        "avatar_display",
+        "recipes_count",
+        "subscriptions_count",
+        "subscribers_count",
     )
     list_display_links = ("id", "username")
     list_filter = ("is_staff", "is_superuser", "is_active", "date_joined")
@@ -57,9 +60,46 @@ class UserAdmin(BaseUserAdmin):
         ),
     )
 
+    def get_queryset(self, request):
+        return (
+            super()
+            .get_queryset(request)
+            .select_related()
+            .prefetch_related(
+                "recipes", "subscriptions", "author_subscriptions"
+            )
+        )
+
+    def full_name(self, obj):
+        return f"{obj.first_name} {obj.last_name}".strip()
+
+    full_name.short_description = "ФИО"
+
+    @mark_safe
+    def avatar_display(self, obj):
+        if obj.avatar:
+            return f'<img src="{obj.avatar.url}" width="50" height="50" style="border-radius: 50%; object-fit: cover;" alt="Аватар {obj.username}">'
+        return '<span style="color: #999;">Нет аватара</span>'
+
+    avatar_display.short_description = "Аватар"
+
+    def recipes_count(self, obj):
+        return obj.recipes.count()
+
+    recipes_count.short_description = "Рецептов"
+
+    def subscriptions_count(self, obj):
+        return obj.subscriptions.count()
+
+    subscriptions_count.short_description = "Подписок"
+
+    def subscribers_count(self, obj):
+        return obj.author_subscriptions.count()
+
+    subscribers_count.short_description = "Подписчиков"
+
 
 @admin.register(Subscription)
 class SubscriptionAdmin(admin.ModelAdmin):
-    list_display = ("id", "user", "author", "created_at")
+    list_display = ("id", "user", "author")
     search_fields = ("user__username", "author__username")
-    list_filter = ("created_at",)
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,56 +1,6 @@
-from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 from django.db import models
-
-
-class UserManager(BaseUserManager):
-    def create_user(
-        self,
-        email,
-        username,
-        first_name="",
-        last_name="",
-        password=None,
-        **extra_fields,
-    ):
-        if not email:
-            raise ValueError("Email адрес обязателен")
-        if not username:
-            raise ValueError("Имя пользователя обязательно")
-
-        email = self.normalize_email(email)
-        user = self.model(
-            email=email,
-            username=username,
-            first_name=first_name,
-            last_name=last_name,
-            **extra_fields,
-        )
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(
-        self,
-        email,
-        username,
-        first_name="",
-        last_name="",
-        password=None,
-        **extra_fields,
-    ):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError("Суперпользователь должен иметь is_staff=True.")
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError(
-                "Суперпользователь должен иметь is_superuser=True."
-            )
-
-        return self.create_user(
-            email, username, first_name, last_name, password, **extra_fields
-        )
 
 
 class User(AbstractUser):
@@ -62,11 +12,16 @@ class User(AbstractUser):
     )
 
     username = models.CharField(
-        "Имя пользователя",
+        "Псевдоним",
         max_length=150,
         unique=True,
         help_text="Обязательное поле. Не более 150 символов. "
         "Только буквы, цифры и символы @/./+/-/_.",
+        validators=[
+            RegexValidator(
+                r"^[\w.@+-]+$", "Введите корректный псевдоним пользователя."
+            )
+        ],
     )
 
     first_name = models.CharField(
@@ -86,14 +41,12 @@ class User(AbstractUser):
     )
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["username", "first_name", "last_name"]
-
-    objects = UserManager()
+    REQUIRED_FIELDS = ("username", "first_name", "last_name")
 
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
-        ordering = ["id"]
+        ordering = ("email",)
 
     def __str__(self):
         return self.username
@@ -101,28 +54,21 @@ class User(AbstractUser):
 
 class Subscription(models.Model):
     user = models.ForeignKey(
-        "User",
+        User,
         on_delete=models.CASCADE,
         related_name="subscriptions",
         verbose_name="Подписчик",
-        db_index=True,
     )
     author = models.ForeignKey(
-        "User",
+        User,
         on_delete=models.CASCADE,
-        related_name="subscribers",
+        related_name="author_subscriptions",
         verbose_name="Автор",
-        db_index=True,
-    )
-    created_at = models.DateTimeField(
-        auto_now_add=True,
-        verbose_name="Дата подписки",
     )
 
     class Meta:
         verbose_name = "Подписка"
         verbose_name_plural = "Подписки"
-        ordering = ["-created_at"]
         constraints = [
             models.UniqueConstraint(
                 fields=["user", "author"], name="unique_subscription"
